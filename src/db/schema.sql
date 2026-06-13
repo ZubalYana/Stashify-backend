5c5a9f1dc261e7371654cbe079ad280a69a832b4
Yana <zubalana0@gmail.com>
2026-06-13 10:02:56 +0300

    modify user database schema to accept name

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() 
